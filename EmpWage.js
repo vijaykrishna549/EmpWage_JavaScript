9a20341b439378629bfec57e67bc84660cb1dddd
pvijaykrishna549 <you@example.com>
2021-06-16 15:44:32 +0530

    Calculating Wages for Month Assuming 20 Days A Month

--- EmpWage.js
+++ EmpWage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const IS_PART_TIME_HOURS = 4;
 const IS_FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck)
 {
@@ -15,10 +16,17 @@ switch(empCheck)
             return IS_FULL_TIME_HOURS;
 
             default:
-                empHrs = 0;
+                return 0;
 }
 }
-let empCheck = Math.floor(Math.random() * 10 % 3);
-let empHrs = getWorkingHours(empCheck)
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours : " +empHrs + " UC3 -  EmpWage is: " + empWage);
+let totalEmpHrs = 0;
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+{
+    let empCheck = Math.floor(Math.random() * 10 % 3);
+    totalEmpHrs += getWorkingHours(empCheck);
+
+}
+
+//let empHrs = getWorkingHours(empCheck)
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log( totalEmpHrs+ "Hours : " + " UC4 -  EmpWage is: " + empWage);
