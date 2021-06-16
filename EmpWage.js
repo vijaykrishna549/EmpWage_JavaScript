2584bd48b50e08773477d0103407d9d9360cd9ac
pvijaykrishna549 <you@example.com>
2021-06-16 06:03:53 +0530

    To Calculate EmpWage Using Switch Cases

--- EmpWage.js
+++ EmpWage.js
@@ -1,9 +1,32 @@
-const IS_ABSENT = 0
+{
+    const IS_ABSENT = 0
 let empCheck = Math.floor(Math.random() * 10 % 2);
 if(empCheck == IS_ABSENT)
 {
-    console.log("Employee is Absent");
+    console.log("UC1 - Employee is Absent");
 }
 else{
-    console.log("Employee is Present");
+    console.log("UC1 - Employee is Present");
 }
+}
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const IS_PART_TIME_HOURS = 4;
+const IS_FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10 % 3);
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        empHrs = IS_PART_TIME_HOURS;
+        break;
+        case IS_FULL_TIME:
+            empHrs = IS_FULL_TIME_HOURS;
+            break;
+            default:
+                empHrs = 0;
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("UC2 - EmpWage is: " + empWage)
